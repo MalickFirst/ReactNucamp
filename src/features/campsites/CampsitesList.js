435c0676e5d893ba5ea648e21e7befed4632b08e
Maliki Ouedraogo <ouedraogomaliki225@gmail.com>
2022-09-06 10:05:44 -0400

    usetate

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,11 +1,12 @@
 import { Col, Row } from "reactstrap";
 import CampsiteCard from "./CampsiteCard";
-import { selectAllCampsites } from './campsitesSlice';
+import { selectAllCampsites } from "./campsitesSlice";
+
 
   
 
-const CampsitesList = () => {
-    const campsites = selectAllCampsites();
+const CampsitesList = ({ setCampsiteId }) => {
+   const campsites = selectAllCampsites();
 
 
 
@@ -13,8 +14,12 @@ const CampsitesList = () => {
          <Row className="ms-auto">
              {campsites.map((campsite) => {
                  return (
-                     <Col md="5" className="m-4" key={campsite.id}>
-                         <CampsiteCard campsite={campsite} />
+                    <Col
+                    md='5'
+                    className='m-4'
+                    key={campsite.id}
+                    onClick={() => setCampsiteId(campsite.id)}>
+                        <CampsiteCard campsite={campsite}  />
                      </Col>
                  );
              })}
