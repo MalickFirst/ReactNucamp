3da0f4cb39f0e882460cdbc98bc33538d9a991c9
Maliki Ouedraogo <ouedraogomaliki225@gmail.com>
2022-09-08 00:55:56 -0400

    Homepage display

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -5,6 +5,13 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
+
+
 export const selectCampsiteById = (id) => {
     return CAMPSITES.find((campsite) => campsite.id === id);
+};
+
+
+export const selectFeaturedCampsite = () => {
+    return CAMPSITES.find((campsite) => campsite.featured);
 };
